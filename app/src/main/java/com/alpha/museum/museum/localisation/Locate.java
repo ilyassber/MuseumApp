007fb7b58f4249c0832252611b428d105323be69
Ilyass Berchida <iberchid@e2r10p14.1337.ma>
2019-07-21 00:17:21 +0100

    start working on indoor location tracker

--- app/src/main/java/com/alpha/museum/museum/localisation/Locate.java
+++ app/src/main/java/com/alpha/museum/museum/localisation/Locate.java
@@ -1,6 +1,43 @@
 package com.alpha.museum.museum.localisation;
 
+import com.alpha.museum.museum.models.Location;
+
 public class Locate {
 
+    void getLocation (Location location) {
 
+        double[] med = new double[2];
+        double[] pt1 = new double[2];
+        double[] pt2 = new double[2];
+        double dist;
+        double dist_a;
+        double dist_x;
+        double dist_y;
+        double h;
+        double alpha;
+        double dist_1;
+        double dist_2;
+
+        dist_x = location.getBeacon_a().getX() - location.getBeacon_b().getX();
+        dist_y = location.getBeacon_a().getY() - location.getBeacon_b().getY();
+        dist = Math.sqrt(Math.pow(dist_x, 2) + Math.pow(dist_y, 2));
+        alpha = location.getDist_a() / dist;
+        med[0] = location.getBeacon_a().getX() - (dist_x * alpha);
+        med[1] = location.getBeacon_a().getY() - (dist_y * alpha);
+        dist_a = dist * alpha;
+        h = Math.sqrt(Math.pow(dist_a, 2) + Math.pow(location.getDist_a(), 2));
+        pt1[0] = med[0] + (h * (location.getBeacon_b().getY() - location.getBeacon_a().getY()) / dist);
+        pt1[1] = med[1] - (h * (location.getBeacon_b().getX() - location.getBeacon_a().getX()) / dist);
+        pt2[0] = med[0] - (h * (location.getBeacon_b().getY() - location.getBeacon_a().getY()) / dist);
+        pt2[1] = med[1] + (h * (location.getBeacon_b().getX() - location.getBeacon_a().getX()) / dist);
+        dist_1 = Math.sqrt(Math.pow((pt1[0] - location.getBeacon_c().getX()), 2) + Math.pow((pt1[1] - location.getBeacon_c().getY()), 2));
+        dist_2 = Math.sqrt(Math.pow((pt2[0] - location.getBeacon_c().getX()), 2) + Math.pow((pt2[1] - location.getBeacon_c().getY()), 2));
+        if (Math.abs(dist_1 - location.getDist_c()) < Math.abs(dist_2 - location.getDist_c())) {
+            location.setX((int) (pt1[0] + 0.5));
+            location.setY((int) (pt1[1] + 0.5));
+        } else {
+            location.setX((int) (pt2[0] + 0.5));
+            location.setY((int) (pt2[1] + 0.5));
+        }
+    }
 }
--- app/src/main/java/com/alpha/museum/museum/models/Location.java
+++ app/src/main/java/com/alpha/museum/museum/models/Location.java
@@ -7,13 +7,19 @@ public class Location implements Serializable {
     private Beacon beacon_a;
     private Beacon beacon_b;
     private Beacon beacon_c;
+    private int dist_a;
+    private int dist_b;
+    private int dist_c;
     private int x;
     private int y;
 
-    public void Location(Beacon beacon_a, Beacon beacon_b, Beacon beacon_c) {
+    public void Location(Beacon beacon_a, Beacon beacon_b, Beacon beacon_c, int dist_a, int dist_b, int dist_c) {
         this.beacon_a = beacon_a;
         this.beacon_b = beacon_b;
         this.beacon_c = beacon_c;
+        this.dist_a = dist_a;
+        this.dist_b = dist_b;
+        this.dist_c = dist_c;
     }
 
     public Beacon getBeacon_a() {
@@ -38,6 +44,30 @@ public class Location implements Serializable {
 
     public void setBeacon_c(Beacon beacon_c) {
         this.beacon_c = beacon_c;
+    }
+
+    public int getDist_a() {
+        return dist_a;
+    }
+
+    public void setDist_a(int dist_a) {
+        this.dist_a = dist_a;
+    }
+
+    public int getDist_b() {
+        return dist_b;
+    }
+
+    public void setDist_b(int dist_b) {
+        this.dist_b = dist_b;
+    }
+
+    public int getDist_c() {
+        return dist_c;
+    }
+
+    public void setDist_c(int dist_c) {
+        this.dist_c = dist_c;
     }
 
     public int getX() {
